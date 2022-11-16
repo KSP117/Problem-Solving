b8e67b6fbf084d6dd3127a0eb2e660750b0e5967
bigmantings <kevshaji@gmail.com>
2022-11-16 12:54:36 +0530

    Added 03_sort_words_by_length

---
+++ 01_easy_algorithms/03_sort_words_by_length.rb
@@ -0,0 +1,11 @@
+def sort_by_length(words)
+  arr_of_words = words.split(" ")
+  arr_of_words.sort!{|x, y| x.length <=> y.length}
+  arr_of_words.join(" ")
+end
+
+print sort_by_length("Hello my friend")
+puts " "
+print sort_by_length("Have a wonderful day")
+puts " "
+print sort_by_length("My son loves pineapples")

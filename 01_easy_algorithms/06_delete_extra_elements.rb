0253fc0b18e2ed3f06c7b321cb577e98e3543f4a
bigmantings <kevshaji@gmail.com>
2022-11-17 11:15:29 +0530

    Added 06_delete_extra_elements

---
+++ 01_easy_algorithms/06_delete_extra_elements.rb
@@ -0,0 +1,22 @@
+def delete_occurrences (arr, num)
+  element_count = Hash.new(0)
+  delete_arr = []
+  new_arr = []
+  arr.each do |n|
+    element_count[n] += 1
+    
+    if element_count[n] > num
+      delete_arr << n
+    else
+      new_arr << n
+    end
+    
+  end
+  new_arr
+end  
+
+print delete_occurrences([1, 1, 1, 1], 2)
+puts ""
+print delete_occurrences([13, true, 13, nil], 1)
+puts ""
+print delete_occurrences([true, true, true], 3)

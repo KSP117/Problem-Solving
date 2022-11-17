48aaedb2a749da71545f135cf2d666a86bcaa92b
bigmantings <kevshaji@gmail.com>
2022-11-17 11:22:33 +0530

    Added 08_recursion_length_of_string

---
+++ 01_easy_algorithms/08_recursion_length_of_string.rb
@@ -0,0 +1,15 @@
+def length(str)
+  
+  return 0 if str == ""
+  
+  return length(str[1..(str.length)]) + 1
+  
+end
+
+print length("apple")
+puts ""
+print length("make")
+puts ""
+print length("a")
+puts ""
+print length("")

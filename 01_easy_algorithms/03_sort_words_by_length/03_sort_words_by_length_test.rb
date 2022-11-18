a524b7eb4fe9f6760aea8eaad3dad77f0294d132
Kevin S.P <115455129+KSP117@users.noreply.github.com>
2022-11-18 13:23:11 +0530

    Update 03_sort_words_by_length_test.rb

--- 01_easy_algorithms/03_sort_words_by_length/03_sort_words_by_length_test.rb
+++ 01_easy_algorithms/03_sort_words_by_length/03_sort_words_by_length_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative '03_sort_words_by_length'
 
-class ValidPhoneNumberTest < Minitest::Test
+class SortWordsByLengthTest < Minitest::Test
 
   def test_if_sort_by_length
     assert sort_by_length("Hello my friend")

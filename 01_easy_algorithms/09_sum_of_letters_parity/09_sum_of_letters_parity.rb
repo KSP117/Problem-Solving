a6a5058d8aed4c8f1caf255a6245bda052e81508
bigmantings <kevshaji@gmail.com>
2022-11-21 16:19:17 +0530

    Updated script and test

--- 01_easy_algorithms/09_sum_of_letters_parity/09_sum_of_letters_parity.rb
+++ 01_easy_algorithms/09_sum_of_letters_parity/09_sum_of_letters_parity.rb
@@ -4,6 +4,3 @@ def is_alpha (x)
   letter_num = letter_arr.map { |d| alphabet.index(d) + 1 }
   letter_num.sum.even? == true ? true : false
 end
-puts is_alpha("I'am king")
-puts is_alpha("True")
-puts is_alpha("alexa")

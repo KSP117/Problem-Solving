e09cb40dce4202f511a7bb95e8508622e6eba5d1
bigmantings <kevshaji@gmail.com>
2022-11-21 16:28:37 +0530

    Updated script and test

--- 01_easy_algorithms/11_free_shipping/11_free_shipping.rb
+++ 01_easy_algorithms/11_free_shipping/11_free_shipping.rb
@@ -1,7 +1,3 @@
 def free_shipping (x)
   x.values.inject {|sum, number| sum + number} < 50 ? false : true
 end
-
-puts free_shipping({ "Shampoo" => 5.99, "Rubber Ducks" => 15.99 })
-puts free_shipping({ "Flatscreen TV" => 399.99 })
-puts free_shipping({ "Monopoly" => 11.99, "Secret Hitler" => 35.99, "Bananagrams" => 13.99 })

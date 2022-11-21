45cab05c41ebd24119192a01934a9d9299bc8c3f
bigmantings <kevshaji@gmail.com>
2022-11-21 16:12:39 +0530

    Updated script and test

--- 01_easy_algorithms/07_recursion_greatest_common_divisor/07_recursion_greatest_common_divisor.rb
+++ 01_easy_algorithms/07_recursion_greatest_common_divisor/07_recursion_greatest_common_divisor.rb
@@ -31,20 +31,3 @@ def gcd_recursion(a, b)
     return a;
   
 end
-
-print gcd(10, 20)
-puts ""
-print gcd(1, 3)
-puts ""
-print gcd(5, 7)
-puts ""
-print gcd(2, 6)
-puts ""
-print gcd_recursion(10, 20)
-puts ""
-print gcd_recursion(1, 3)
-puts ""
-print gcd_recursion(5, 7)
-puts ""
-print gcd_recursion(2, 6)
-puts ""
--- 01_easy_algorithms/07_recursion_greatest_common_divisor/07_recursion_greatest_common_divisor_test.rb
+++ 01_easy_algorithms/07_recursion_greatest_common_divisor/07_recursion_greatest_common_divisor_test.rb
@@ -4,8 +4,8 @@ require_relative '07_recursion_greatest_common_divisor'
 class RecursionGreatestCommomDivisorTest < Minitest::Test
   
   def test_greatest_common_divisor
-    assert gcd(10, 20)
-    gcd_recursion(10, 20)
+    assert_equal gcd(10, 20), 10
+    assert_equal gcd_recursion(10, 20), 10
   end
 
 end

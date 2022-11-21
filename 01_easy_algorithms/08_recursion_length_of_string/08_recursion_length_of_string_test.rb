7333dc752ca09ccdb8f4dbdbaef4bd927a3acfa7
bigmantings <kevshaji@gmail.com>
2022-11-21 16:16:50 +0530

    Updated script and test

--- 01_easy_algorithms/08_recursion_length_of_string/08_recursion_length_of_string_test.rb
+++ 01_easy_algorithms/08_recursion_length_of_string/08_recursion_length_of_string_test.rb
@@ -4,7 +4,7 @@ require_relative '08_recursion_length_of_string'
 class RecursionTest < Minitest::Test
   
   def test_if_word_count_gets_caputured
-    assert length("apple")
+    assert_equal length("apple"), 5
   end
 
 end

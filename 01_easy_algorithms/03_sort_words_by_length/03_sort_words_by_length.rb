db601881bb82a18ee544b8af0e99e37323a79c58
Kevin S.P <115455129+KSP117@users.noreply.github.com>
2022-11-21 14:45:29 +0530

    Update 03_sort_words_by_length.rb

--- 01_easy_algorithms/03_sort_words_by_length/03_sort_words_by_length.rb
+++ 01_easy_algorithms/03_sort_words_by_length/03_sort_words_by_length.rb
@@ -3,10 +3,3 @@ def sort_by_length(words)
   arr_of_words.sort!{|x, y| x.length <=> y.length}
   arr_of_words.join(" ")
 end
-
-print sort_by_length("Hello my friend")
-puts
-print sort_by_length("Have a wonderful day")
-puts
-print sort_by_length("My son loves pineapples")
-puts

d8b195e17ae1790d9497f0e796defa305983c490
bigmantings <kevshaji@gmail.com>
2022-11-21 14:51:09 +0530

    Updated script and test

--- 01_easy_algorithms/04_sharing_is_caring/04_sharing_is_caring.rb
+++ 01_easy_algorithms/04_sharing_is_caring/04_sharing_is_caring.rb
@@ -2,9 +2,3 @@ def show_the_love(x)
   diff = x.map { |n| n*0.25 unless n == x.min }.compact!.sum
   x.map { |n| n*0.75 unless n == x.min }.map { |n| n == nil ? x.min+diff : n}
 end
-
-print show_the_love([4, 1, 4])
-puts " "
-print show_the_love([16, 10, 8])
-puts ""
-print show_the_love([2, 100])
--- 01_easy_algorithms/04_sharing_is_caring/04_sharing_is_caring_test.rb
+++ 01_easy_algorithms/04_sharing_is_caring/04_sharing_is_caring_test.rb
@@ -4,7 +4,9 @@ require_relative '04_sharing_is_caring'
 class SharingIsCaringTest < Minitest::Test
 
   def test_if_percentage_gets_reduced
-    assert show_the_love([4, 1, 4])
+    assert_equal show_the_love([4, 1, 4]), [3.0, 3.0, 3.0] 
+    assert_equal show_the_love([16, 10, 8]), [12.0, 7.5, 14.5]
+    assert_equal show_the_love([2, 100]), [27.0, 75.0]  
   end
 
 end

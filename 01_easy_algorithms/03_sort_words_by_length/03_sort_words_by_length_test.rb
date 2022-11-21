6e484b14c239defd8f84a53473a1fe733b4c2eaf
Kevin S.P <115455129+KSP117@users.noreply.github.com>
2022-11-21 14:44:50 +0530

    Update 03_sort_words_by_length_test.rb

--- 01_easy_algorithms/03_sort_words_by_length/03_sort_words_by_length_test.rb
+++ 01_easy_algorithms/03_sort_words_by_length/03_sort_words_by_length_test.rb
@@ -4,7 +4,7 @@ require_relative '03_sort_words_by_length'
 class SortWordsByLengthTest < Minitest::Test
 
   def test_if_sort_by_length
-    assert sort_by_length("Hello my friend")
+    assert_equal sort_by_length("Hello my friend"), "my Hello friend"
   end
 
 end  

a3731ac5627dff3b01f1c17eafe9bd455114c788
bigmantings <kevshaji@gmail.com>
2022-11-21 14:17:12 +0530

    Updated script and test

--- 01_easy_algorithms/02_merge_arrays/02_merge_arrays.rb
+++ 01_easy_algorithms/02_merge_arrays/02_merge_arrays.rb
@@ -22,10 +22,3 @@ def merge_arrays(x, y)
   merged
   
 end
-
-print merge_arrays(["y", "b", "d", "r", "e"], [1, 8, 3, 9, 5])
-puts ''
-print merge_arrays([1, 2, 3], ["a", "b", "c", "d", "e", "f"])
-puts ''
-print merge_arrays(["f", "d", "w", "t"], [5, 3, 7, 8])
-puts
